7905f5cfb9d222eb0c666acad39279dc9384d97c
Reid Baldwin & Kyle Bulloch <student@epicodus.com>
2015-04-16 09:59:56 -0700

    Add multiple address functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,19 @@ jQuery(document).ready(function() {
     var firstName = $("input#first").val();
     var lastName = $("input#last").val();
     var phone = $("input#phone").val();
-    var street = $("input#street").val();
-    var city = $("input#city").val();
-    var state = $("input#state").val();
-    var zip = $("input#zip").val();
     var contact = { firstName: firstName, lastName: lastName, phone: phone,
-      street: street, city: city, state: state, zip: zip,
-      fullAddress: function() {
-        return (this.street + " " + this.city + ", " + this.state + " " + this.zip);
-      }
+      addresses: []
     };
+
+    $(".new-address").each(function() {
+      var street = $(this).find("input.new-street").val();
+      var city = $(this).find("input.new-city").val();
+      var state = $(this).find("input.new-state").val();
+      var zip = $(this).find("input.new-zip").val();
+
+      var newAddress = { street: street, city: city, state: state, zip: zip };
+      contact.addresses.push(newAddress);
+    });
 
     $("ul#contact-list").append("<li><span class='contact'>" +
       contact.firstName + " " + contact.lastName + "</span></li>");
@@ -25,16 +28,21 @@ jQuery(document).ready(function() {
       $('.first-name').text(contact.firstName);
       $('.last-name').text(contact.lastName);
       $('.phone').text(contact.phone);
-      $('.address').text(contact.fullAddress());
+
+      $('ul#addresses').text("");
+      contact.addresses.forEach(function(address) {
+        $('ul#addresses').append("<li>" + address.street + ", " + address.city +
+        ", " + address.state + " " + address.zip + "</li>");
+      });
     });
 
     $("input#first").val("");
     $("input#last").val("");
     $("input#phone").val("");
-    $("input#street").val("");
-    $("input#city").val("");
-    $("input#state").val("");
-    $("input#zip").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+    $("input.new-zip").val("");
 
     $('#contact-list').show();
     $('#contacts-list').show();
@@ -53,6 +61,9 @@ jQuery(document).ready(function() {
          '</div>' +
          '<div class="form-group">' +
            '<input type="text" class="form-control new-state" placeholder="New State">' +
+         '</div>' +
+         '<div class="form-group">' +
+           '<input type="number" class="form-control new-zip" placeholder="New Zip">' +
          '</div>' +
        '</div>');
   });
